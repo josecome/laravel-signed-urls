2d623d2e861d7c1647c315facc1cb20aeefa5f6a
Jose Jaime Come <josejaimecome@gmail.com>
2023-09-18 18:38:45 +0200

    Temporary signed link added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//When access this link the result will be something like:
+//http://127.0.0.1:8000/file/1?expires=1695056777&signature=878ccd9fd7e3f89afd3b2848d2fa6453db2c9c5f1a3735e99582fc66f67aab05
+Route::get('/download', function () {
+    return URL::temporarySignedRoute(
+        'file', now()->addMinutes(30), ['id' => 1]
+    );
+});
+
+//From the link generated above, this route will be called
+Route::get('/file/{id}', function (Request $request) {
+    if (! $request->hasValidSignature()) {
+        abort(401);
+    }
+    return "Good";
+})->name('file');
+
+
